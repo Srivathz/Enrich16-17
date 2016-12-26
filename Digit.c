b2186efe21c2ab92206ede16cd1fc8af97a3e3cd
Srivathsan <samsrivaths@gmail.com>
2016-12-26 20:55:55 +0530

    Create Digit.c

---
+++ Digit.c
@@ -0,0 +1,16 @@
+//# To print sum of digits
+#include<stdio.h>
+
+void main()
+{
+    int num,sum=0,dig=0,i;
+    printf("Enter a number");
+    scanf("%d",&num);
+    for(i=num;i>0;i/=10)
+    {
+        t=i%10;
+        sum+=t;
+        dig++;
+    }
+    printf("\n Digits=%d \n Sum of digits=%d",dig,sum);
+}    
