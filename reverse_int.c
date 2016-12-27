28b1aa1be458bbf156190994aada60f06c3c3384
Srivathsan <samsrivaths@gmail.com>
2016-12-27 18:52:58 +0530

    Create reverse_int.c

---
+++ reverse_int.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    long int num,rev=0,i,d=0;
+    printf("Enter an integer ");
+    scanf("%ld",&num);
+    if(num<0)
+        i=abs(num);
+    else 
+        i=num;
+    do
+    {
+        d=i%10;
+        rev=rev*10+d;
+        i/=10;
+    }while(i>0);
+    if(num>0)
+        printf("\n The reverse is: %ld",rev);
+    else if(num<0)
+        printf("\n The reverse is: -%ld",rev);
+    else
+        printf("\n The reverse is: 0");
+}     
