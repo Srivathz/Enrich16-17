b2ca0e0cc273dd78d8a678372b86d5b2f567fc94
Srivathsan <samsrivaths@gmail.com>
2016-12-28 19:06:47 +0530

    Create shift.c

---
+++ shift.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void main()
+{
+    long int num,shift,rem;
+    int m,i;
+    printf("Enter a 5 digit number : ");
+    scanf("%ld",&num);
+    printf("\nNo. of times to rotate : ");
+    scanf("%d",&m);
+    if((num<10000)||(num>99999))
+        printf("\n Not a 5 digit number);
+    else
+    {
+        m=m%5;
+        for(i=0;i<m;i++)
+        { 
+            rem=num%10;
+            shift=num/10;
+            num=(rem*10000)+shift;
+        } 
+        printf("\n %ld ",num);
+    }
+}
