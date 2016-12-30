7edc882456827aa1f0bd956418447798b0be3245
Srivathsan <samsrivaths@gmail.com>
2016-12-30 14:15:01 +0530

    Create strpalin.c

---
+++ strpalin.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+    char str[100],rev[100];
+    int i,j;
+    printf("Enter a string");
+    scanf("%s",str);
+    j=strlen(str);
+    for(i=0;i<j;i++)
+    	rev[i]=str[j-i-1];
+    rev[i]='\0';
+    if(strcmp(str,rev)==0)
+        printf("\n The given string is a palindrome");
+    else
+        printf("\n The given string is not a palindrome");
+}
