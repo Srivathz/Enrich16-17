e91416fedd729f01d2f721f745ff1887b455ffae
Srivathsan <samsrivaths@gmail.com>
2016-12-30 15:26:11 +0530

    Create strmanip.c

---
+++ strmanip.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main()
+{
+    char str[100],mod[100];
+    int i,j,k=0;
+    printf("Enter a string");
+    gets(str);
+    j=strlen(str);
+    for(i=0;i<j;i++)
+    {
+        if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]==' '||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U');
+        else
+        {
+            mod[k]=str[i];
+            k++;
+        }    
+    }
+    mod[k]='\0';
+    printf("\n%s",mod);    
+}
