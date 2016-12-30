94cd1af388a6449e03adf0aa4737ba3fc302dbd4
Srivathsan <samsrivaths@gmail.com>
2016-12-30 13:40:57 +0530

    Create matfill.c

---
+++ matfill.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void main()
+{
+	int a[10][10],m,n,k,i,j;
+	printf("Enter M, N and K");
+	scanf("%d%d%d",&m,&n,&k);
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if((i+j)==k)
+				a[i][j]=1;
+			else
+			    a[i][j]=0;
+		}
+	}
+	for(i=0;i<m;i++)
+	{
+		printf("\n");
+		for(j=0;j<n;j++)
+			printf("%d   ",a[i][j]);
+	}
+}
