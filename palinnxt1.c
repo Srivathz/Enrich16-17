d66dd6cf83309f5a1e65a60cde88541df784f031
Srivathsan <samsrivaths@gmail.com>
2017-01-31 23:50:30 +0530

    Update palinnxt1.c

--- palinnxt1.c
+++ palinnxt1.c
@@ -3,7 +3,7 @@
 
 void main()
 {
-	int num,d=0,i,palin=0,dig[10],x;
+    int num,d=0,i,palin=0,dig[10],x;
     printf("Enter the number :");
     scanf("%d",&num);
     for(i=num;i>0;i/=10)
@@ -15,30 +15,30 @@ void main()
     if(d%2==0)
     	i=0;
     else
-		i=1;	
-	if(dig[x+i]<dig[x-1])
+	i=1;	
+    if(dig[x+i]<dig[x-1])
     {
     	dig[x]++;
-	}
-	else if(dig[x+i]==dig[x-1])
+    }
+    else if(dig[x+i]==dig[x-1])
+    {
+	if(dig[x+1+i]<=dig[x-2])
 	{
-		if(dig[x+1+i]<=dig[x-2])
-		{
-			dig[x]++;
-		}	
-		else
-		{
-			dig[x-2]=dig[x+1+i];
-		}	
+	    dig[x]++;
+	}	
+	else
+	{
+            dig[x-2]=dig[x+1+i];
 	}
+    }
 
-	for(i=d-1;i>=d/2;i--)
+    for(i=d-1;i>=d/2;i--)
     {
         palin+=dig[i]*pow(10,i);
         if(i!=(d-1-i))
         {
-			palin+=dig[i]*pow(10,d-1-i);
-		}	
+	    palin+=dig[i]*pow(10,d-1-i);
+	}	
     }
     printf("\n The next Palindrome is :%d",palin);	
 }
